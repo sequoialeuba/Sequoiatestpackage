11d5220485da3cced30c949ca9be36783a73e09a
Sequoia <86361780+sequoialeuba@users.noreply.github.com>
2026-07-14 14:22:55 +0100

    updates the ggplot specifics

---
+++ R/Sequoiatestpackage.R
@@ -0,0 +1,7 @@
+#' Sequoiatestpackage: this package runs foo and ggplot2 for quick test set up
+#'
+#' This could be longer to develop this 
+#' main problem it solves.
+#'
+#' @keywords internal
+"_PACKAGE"
--- R/main.R
+++ R/main.R
@@ -6,11 +6,15 @@
 #' @return Invisibly returns \code{NULL}. Called for its side effect of
 #'   printing to the console.
 #' @export
-#' @import ggplot2
+#' @importFrom ggplot2 ggplot theme_bw geom_point aes
 #' @examples
 #' foo()
 
 foo <- function() {
+  
+  # deal with no visible binding issue
+  x <- NULL
+  
   data.frame(x <- 1:10) |>
     ggplot() + theme_bw() + geom_point(aes(x=x, y=1))
   
